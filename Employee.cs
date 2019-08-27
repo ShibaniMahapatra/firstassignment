e83357484f8476fffaed963169c0a8a57d2f10ba
Shibani Mahapatra <34315569+ShibaniMahapatra@users.noreply.github.com>
2019-08-28 00:02:18 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpAge { get; set; }
+
+        public double EmpSalary { get; set; }
+
+      
+    }
+}
